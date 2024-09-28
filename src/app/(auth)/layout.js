cf83aebd2ab5c963bd3e8a05ed2624e168bce49d
Sata Lesmana <lesmanasata@gmail.com>
2024-09-28 11:01:04 +0700

    auth page

--- src/app/(auth)/layout.js
+++ src/app/(auth)/layout.js
@@ -16,15 +16,12 @@ export default function RootLayout({ children }) {
             <div className="max-w-5xl mx-auto px-6 md:px-12 xl:px-6">
               <div className="flex flex-wrap items-center justify-between">
                 <div>
-                  <a href="/">LOGO</a>
+                  <a href="/admin">Admin Panel</a>
                 </div>
                 <div>
                   <ul className="flex flex-row gap-6">
                     <li>
-                      <a href="/about">asdfasdf</a>
-                    </li>
-                    <li>
-                      <a href="/work">Work</a>
+                      <a href="/admin/work">Work</a>
                     </li>
                   </ul>
                 </div>
