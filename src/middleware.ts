fbdc143aad852e70b4d72ca2c406416cbb05fa03
Sata Lesmana <lesmanasata@gmail.com>
2024-11-11 18:00:25 +0700

    token perivy

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,5 +1,6 @@
 import type { NextRequest } from 'next/server'
 import {  NextResponse } from 'next/server'
+import {  jwtVerify } from 'jose'
 
 const protectedRoutes = [
     '/admin',
@@ -8,13 +9,29 @@ const protectedRoutes = [
 ]
 
 export default async function middleware(req: NextRequest) {
-    const cookie = req.cookies.get(`${process.env.AUTH_COOKIE_NAME}`)
-    const path = req.nextUrl.pathname
-    const isProtectedRoute = protectedRoutes.includes(path)
 
-    if (isProtectedRoute && !cookie) {
-        return NextResponse.redirect(new URL('/login', req.nextUrl))
-    }
+        const cookie = await req.cookies.get(`${process.env.AUTH_COOKIE_NAME}`) || ''
+        const path = req.nextUrl.pathname
+        const isProtectedRoute = protectedRoutes.includes(path)
+        let userId:any = ''
 
-    return NextResponse.next()
+        if(cookie?.value){
+            const secretKey = process.env.SESSION_SECRET
+            const encodedKey = new TextEncoder().encode(secretKey)
+            try{
+                const { payload } = await jwtVerify(cookie?.value, encodedKey, {
+                    algorithms: ['HS256'],
+                })
+                userId = payload.id || ''
+            }catch(er){
+                userId = ""
+            }
+        }
+
+        if (isProtectedRoute &&  userId ==='') {
+            return NextResponse.redirect(new URL('/login', req.nextUrl))
+        }
+
+        return NextResponse.next()
+   
 }
