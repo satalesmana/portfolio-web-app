92d0f4ad84ec33800ee10ab8c5758eca856b0b7b
Sata Lesmana <lesmanasata@gmail.com>
2024-11-18 19:52:09 +0700

    Ahmad Zaki

--- src/app/(auth)/admin/blogs/page.js
+++ src/app/(auth)/admin/blogs/page.js
@@ -1,7 +1,45 @@
+import Card from '../../../../components/card';
 export default function AdminBlogs() {
     return (
-      <>
-        asdf
+        <>
+        <Card title="List of Blogs" style="mt-5">
+            <table className="table-auto">
+                <thead>
+                    <tr>
+                        <th className='p-2 border-b border-blue-gray-100 bg-gray-100'>Title</th>
+                        <th className='p-2 border-b border-blue-gray-100 bg-gray-100'>Sub_Title</th>
+                        <th className='p-2 border-b border-blue-gray-100 bg-gray-100'>Content</th>
+                        <th className='p-2 border-b border-blue-gray-100 bg-gray-100'>Created_at</th>
+                        <th className='p-2 border-b border-blue-gray-100 bg-gray-100'>Message</th>
+                        <th className='p-2 border-b border-blue-gray-100 bg-gray-100'>Action</th>
+                    </tr>
+                </thead>
+                <tbody>
+               
+            
+                    <tr className='border-b border-blue-gray-50 '>
+                        <td className='p-2 '>3</td>
+                        <td className='p-2 '>Jhon doe</td>
+                        <td className='p-2 '>jhondoe@mail.com</td>
+                        <td className='p-2 '>Loremipsum</td>
+                        <td className='p-2 '>loremipsum loremipsum loremipsum</td>
+                        <td className='p-2 '>
+                            <div class="inline-flex text-[12px]">
+                                <button class=" bg-green-300 hover:bg-green-400 text-gray-800 py-2 px-4 rounded-l">
+                                    Detail
+                                </button>
+                                <button class="bg-gray-300 hover:bg-gray-400 text-gray-800 py-2 px-4">
+                                    Edit
+                                </button>
+                                <button class="bg-red-300 hover:bg-red-400 text-gray-800 py-2 px-4 rounded-r">
+                                    Delete
+                                </button>
+                            </div>
+                        </td>
+                    </tr>
+                </tbody>
+            </table>
+        </Card>
       </>
     );
 }
