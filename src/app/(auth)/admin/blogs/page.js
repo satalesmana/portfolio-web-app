b5daf8fc57e9a95a8cc5668ef9cd45c0465ee5e0
Sata Lesmana <lesmanasata@gmail.com>
2024-11-25 21:13:17 +0700

    create blog

--- src/app/(auth)/admin/blogs/page.js
+++ src/app/(auth)/admin/blogs/page.js
@@ -1,51 +1,86 @@
 'use client'
 import Card from '../../../../components/card';
-import { useRouter } from 'next/navigation'
+import { useRouter } from 'next/navigation';
+import { useEffect, useState } from 'react';
+import ConfigDialog from '../../../../components/ConfirmDialog'
 
 export default function AdminBlogs() {
     const router = useRouter();
+    const [modal, setModal] = useState(false)
+    const [modalTitle, setModalTitle] = useState("")
+    const [modalMessage, setModalMessage] = useState("")
+    const [blogs, setBologs] = useState([])
 
     const onAddNew = ()=>{
         router.push('/admin/blogs/form')
     }
+
+    const fetchData = async ()=>{
+        try{
+            const res = await fetch('/api/blogs');
+            let responseData = await res.json()
+            setBologs(responseData.data)
+
+        }catch(err){
+            console.error("ERR", err.message)
+            setModal(true)
+            setModalTitle('Err')
+            setModalMessage(err.message)
+        }
+    }
+
+    useEffect(()=>{
+        fetchData()
+    },[])
+
     return (
         <>
         <Card title="List of Blogs" style="mt-5" showAddBtn onAddNew={onAddNew}>
-            <table className="table-auto">
+            <table className="table-auto w-full">
                 <thead>
                     <tr>
-                        <th className='p-2 border-b border-blue-gray-100 bg-gray-100'>Title</th>
-                        <th className='p-2 border-b border-blue-gray-100 bg-gray-100'>Sub_Title</th>
-                        <th className='p-2 border-b border-blue-gray-100 bg-gray-100'>Content</th>
-                        <th className='p-2 border-b border-blue-gray-100 bg-gray-100'>Created_at</th>
-                        <th className='p-2 border-b border-blue-gray-100 bg-gray-100'>Action</th>
+                        <th className='table-head border-blue-gray-100'>No</th>
+                        <th className='table-head border-blue-gray-100'>Title</th>
+                        <th className='table-head border-blue-gray-100'>Sub Title</th>
+                        <th className='table-head border-blue-gray-100'>Action</th>
                     </tr>
                 </thead>
                 <tbody>
-               
-            
-                    <tr className='border-b border-blue-gray-50 '>
-                        <td className='p-2 '>3</td>
-                        <td className='p-2 '>Jhon doe</td>
-                        <td className='p-2 '>jhondoe@mail.com</td>
-                        <td className='p-2 '>Loremipsum</td>
-                        <td className='p-2 '>
-                            <div className="inline-flex text-[12px]">
-                                <button className=" bg-green-300 hover:bg-green-400 text-gray-800 py-2 px-4 rounded-l">
-                                    Detail
-                                </button>
-                                <button className="bg-gray-300 hover:bg-gray-400 text-gray-800 py-2 px-4">
-                                    Edit
-                                </button>
-                                <button className="bg-red-300 hover:bg-red-400 text-gray-800 py-2 px-4 rounded-r">
-                                    Delete
-                                </button>
-                            </div>
-                        </td>
-                    </tr>
+                    { blogs.map((item, key)=>{
+                        return (
+                            <tr key={key} className='border-b border-blue-gray-50 '>
+                                <td className='p-2 '>{key+1}</td>
+                                <td className='p-2 '>{item.title} </td>
+                                <td className='p-2 '>{item.subTitle} </td>
+                                <td className='p-2 '>
+                                    <div className="inline-flex text-[12px]">
+                                        <button className=" bg-green-300 hover:bg-green-400 text-gray-800 py-2 px-4 rounded-l">
+                                            Detail
+                                        </button>
+                                        <button className="bg-gray-300 hover:bg-gray-400 text-gray-800 py-2 px-4">
+                                            Edit
+                                        </button>
+                                        <button className="bg-red-300 hover:bg-red-400 text-gray-800 py-2 px-4 rounded-r">
+                                            Delete
+                                        </button>
+                                    </div>
+                                </td>
+                            </tr>
+                        )
+                    })
+                    }
                 </tbody>
             </table>
         </Card>
+
+        <ConfigDialog  
+            onOkOny={()=>onCancel()} 
+            showDialog={modal}
+            title={modalTitle}
+            message={modalMessage}
+            onCancel={()=>onCancel()} 
+            onOk={()=>onCancel()} 
+            isOkOnly={true} />
       </>
     );
 }
