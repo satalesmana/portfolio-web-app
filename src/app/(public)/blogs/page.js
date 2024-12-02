1ec520064341a4624836cd946987dafff48709a3
Sata Lesmana <lesmanasata@gmail.com>
2024-12-02 20:49:35 +0700

    detail page

--- src/app/(public)/blogs/page.js
+++ src/app/(public)/blogs/page.js
@@ -1,10 +1,8 @@
 "use client"
 import { useState, useEffect } from 'react'
+import { useRouter } from 'next/navigation'
 
-const CardItem = ({
-    title,
-    subTitle
-})=>{
+const CardItem = ({ title, subTitle})=>{
     return (
         <div className="cursor-pointer hover:drop-shadow-lg drop-shadow-md bg-[#E5E5E5] w-[310px] h-[474px]">
             <div className="flex justify-center">
@@ -25,19 +23,19 @@ const CardItem = ({
 
 const LoadingCard=()=>{ 
     return (
-        <div class="w-[310px] h-[474px] border border-blue-300 shadow rounded-md p-4 max-w-sm w-full mx-auto">
-            <div class="animate-pulse ">
-                <div class=" bg-slate-200 h-[300px]"></div>
-                <div class="flex-1 space-y-6 py-1">
-                    <div class="h-3 bg-slate-200 rounded"></div>
-                    <div class="space-y-2">
-                        <div class="grid grid-cols-3 gap-4">
-                        <div class="h-3 bg-slate-200 rounded col-span-2"></div>
-                        <div class="h-3 bg-slate-200 rounded col-span-1"></div>
+        <div className="w-[310px] h-[474px] border border-blue-300 shadow rounded-md p-4 max-w-sm w-full mx-auto">
+            <div className="animate-pulse ">
+                <div className=" bg-slate-200 h-[300px]"></div>
+                <div className="flex-1 space-y-6 py-1">
+                    <div className="h-3 bg-slate-200 rounded"></div>
+                    <div className="space-y-2">
+                        <div className="grid grid-cols-3 gap-4">
+                        <div className="h-3 bg-slate-200 rounded col-span-2"></div>
+                        <div className="h-3 bg-slate-200 rounded col-span-1"></div>
                         </div>
-                        <div class="h-3 bg-slate-200 rounded"></div>
+                        <div className="h-3 bg-slate-200 rounded"></div>
                     </div>
-                    <div class="h-3 bg-slate-200 rounded"></div>
+                    <div className="h-3 bg-slate-200 rounded"></div>
                 </div>
             </div>
         </div>
@@ -45,6 +43,7 @@ const LoadingCard=()=>{
 }
 
 export default function Blogs(){
+    const router = useRouter();
     const [data, setData] = useState([])
     const [isLoading, setLoading] = useState(true)
 
@@ -79,11 +78,14 @@ export default function Blogs(){
                 { isLoading && <LoadingCard/> }
                 { isLoading && <LoadingCard/> }
 
-                {  data.map((item, key)=><div> <CardItem 
-                        className="m-5 p-4 " 
-                        title={item.title}
-                        subTitle={item.subTitle}
-                        key={key} />
+                {  data.map((item, key)=><div 
+                        onClick={()=>router.push(`/blogs/${item._id}`)}
+                        key={key}> 
+                            <CardItem 
+                                className="m-5 p-4 " 
+                                title={item.title}
+                                subTitle={item.subTitle}
+                                />
                         </div>
                     )
                 }
